4690edc692036bb030a6f30f9eebcd0603312aee
unknown <dmagill89@gmail.com>
2020-05-17 15:35:38 -0700

    adding the SWV HelloWorld and the start of the STM32f407 drivers

---
+++ stm32f407_drivers/Debug/Src/main.d
@@ -0,0 +1,4 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/David/Embdedded\ Programming/STM32/stm32f407_drivers/drivers/Inc/stm32f407.h
+
+C:/Users/David/Embdedded\ Programming/STM32/stm32f407_drivers/drivers/Inc/stm32f407.h:
---
+++ stm32f407_drivers/drivers/Inc/stm32f407.h
@@ -0,0 +1,24 @@
+/*
+ * stm32f407.h
+ *
+ *  Created on: May 17, 2020
+ *      Author: David
+ */
+
+#ifndef INC_STM32F407_H_
+#define INC_STM32F407_H_
+
+/**
+ * base addresses of Flash and SRAM memories
+ */
+
+#define FLASH_BASEADDR				0x08000000U
+#define SRAM1_BASEADDR				0x20000000U
+#define SRAM2_BASEADDR				0x2001C000U
+#define ROM_BASEADDR				0x1FFF0000U
+#define SRAM						SRAM1_BASEADDR
+
+
+
+
+#endif /* INC_STM32F407_H_ */
