d5900d1183bcd82447b0aab6e6330702faf4bf48
unknown <dmagill89@gmail.com>
2020-05-18 21:15:16 -0700

    adding base addresses for AHB and APB peripherals

--- stm32f407_drivers/drivers/Inc/stm32f407.h
+++ stm32f407_drivers/drivers/Inc/stm32f407.h
@@ -18,7 +18,15 @@
 #define ROM_BASEADDR				0x1FFF0000U
 #define SRAM						SRAM1_BASEADDR
 
+/**
+ * AHBx and APBx Bus peripheral base addresses
+ */
 
+#define PERIPH_BASE					0x40000000U
+#define APB1PERIPH_BASE				PERIPH_BASE
+#define APB2PERIPH_BASE				0x40010000U
+#define AHB1PERIPH_BASE				0x40020000U
+#define AHB2PERIPH_BASE				0x50000000U
 
 
 #endif /* INC_STM32F407_H_ */
