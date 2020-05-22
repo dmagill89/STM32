0c7a6f2414d888ea410ae85126d4e0a189fffb1a
unknown <dmagill89@gmail.com>
2020-05-22 16:05:13 -0700

    make spacing consistent

--- stm32f407_drivers/drivers/Inc/stm32f407.h
+++ stm32f407_drivers/drivers/Inc/stm32f407.h
@@ -143,14 +143,14 @@ typedef struct {
  * clock enable macros
  */
 
-#define GPIOA_PCLK_EN()   (RCC->AHB1ENR |= (1 << 0))
-#define GPIOB_PCLK_EN()   (RCC->AHB1ENR |= (1 << 1))
-#define GPIOC_PCLK_EN()   (RCC->AHB1ENR |= (1 << 2))
-#define GPIOD_PCLK_EN()   (RCC->AHB1ENR |= (1 << 3))
-#define GPIOE_PCLK_EN()   (RCC->AHB1ENR |= (1 << 4))
-#define GPIOF_PCLK_EN()   (RCC->AHB1ENR |= (1 << 5))
-#define GPIOG_PCLK_EN()   (RCC->AHB1ENR |= (1 << 6))
-#define GPIOH_PCLK_EN()   (RCC->AHB1ENR |= (1 << 7))
-#define GPIOI_PCLK_EN()   (RCC->AHB1ENR |= (1 << 8))
+#define GPIOA_PCLK_EN()             (RCC->AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN()             (RCC->AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN()             (RCC->AHB1ENR |= (1 << 2))
+#define GPIOD_PCLK_EN()             (RCC->AHB1ENR |= (1 << 3))
+#define GPIOE_PCLK_EN()             (RCC->AHB1ENR |= (1 << 4))
+#define GPIOF_PCLK_EN()             (RCC->AHB1ENR |= (1 << 5))
+#define GPIOG_PCLK_EN()             (RCC->AHB1ENR |= (1 << 6))
+#define GPIOH_PCLK_EN()             (RCC->AHB1ENR |= (1 << 7))
+#define GPIOI_PCLK_EN()             (RCC->AHB1ENR |= (1 << 8))
 
 #endif /* INC_STM32F407_H_ */
